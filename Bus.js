8b74fd8f1f38a47446a454ff95fdf2718294793a
nicholashutter <nicholas.hutter@live.com>
2021-12-23 16:36:17 -0600

    able to parse assemly input correctly, bus class redefined

---
+++ Bus.js
@@ -0,0 +1,17 @@
+class Bus
+{
+    #bus = 0;
+
+
+    get getBus()
+    {
+    return this.#bus;
+    }
+    set setBus(value)
+    {
+        this.#bus = value; 
+    }
+}
+
+
+
--- Main.js
+++ Main.js
@@ -1,6 +1,15 @@
 
 //create virtual ram array to hold program to be executed
 var ram = new Array(0xFFFF);
+//hard coded assembly code for initial test case
+ram[0x80E8] = "LDA #01" 
+ram[0x80E9] = "ADC #02"     
+ram[0x80FA] = "STA $0402"    
+
+
+
+
+
 
 var PC; 
 var S; 
@@ -8,16 +17,24 @@ var P;
 var A; 
 var X; 
 var Y; 
+var outofBounds = false; 
+var bus; 
 
+bus = ram[0x80E8];
+
+fetch();
 
 function fetch()
 {
-
+    let Z = bus; 
+    decode(Z);
 }
 
-function decode()
+function decode(Z)
 {
-
+    const split =Z.toString().split(" ");
+    console.log(split[0]);
+    console.log(split[1]);
 }
 
 function execute()
